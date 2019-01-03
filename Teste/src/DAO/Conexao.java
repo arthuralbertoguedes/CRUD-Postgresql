35ce0dbbd9e03b3ab4014fffb241a01434e2e31a
arthuralbertoguedes <agalbertoguedes@gmail.com>
2019-01-03 11:53:11 -0200

    Adicionando projeto completo

---
+++ Teste/src/DAO/Conexao.java
@@ -0,0 +1,29 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+	
+	 private String driver = "org.postgresql.Driver";
+     private String user = "postgres";
+     private String senha = "admin";
+     private String url = "jdbc:postgresql://localhost:5432/Agenda";
+     private static Connection con = null;
+     
+
+ 	public Connection conectar() {
+ 		try {
+ 			Class.forName(driver);
+ 			Connection conexao = null;
+ 			conexao = DriverManager.getConnection(url,user,senha);
+ 			con=conexao;
+ 			return con;
+ 		} catch (Exception e) {
+ 			e.printStackTrace();
+ 			System.out.println(e.getMessage());
+ 			return null;
+ 		}
+      }
+      
+}
